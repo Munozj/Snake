7d6e836abf7cacdcba17943bf180dd76706761f2
Joshua Sean Munoz <joshua273.jm@gmail.com>
2014-10-21 11:19:49 -0700

    3 videos

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -1,14 +1,33 @@
+/* ---------------------------------------------------------------------------
+ * Variables
+ * ---------------------------------------------------------------------------
+ */
+
 var snake; 
 var snakeLength;
 var snakeSize;
+var snakeDirection;
+
+var food;
 
 var context;
 var screenWidth;
 var screenHeight;
 
-gameInitialize();
-gameDraw();
+/* ---------------------------------------------------------------------------
+ * Executing game code
+ * ---------------------------------------------------------------------------
+ */
 
+gameInitialize();
+foodInitialize();
+snakeInitialize();
+setInterval(gameLoop, 1000/30);
+
+/* ---------------------------------------------------------------------------
+ * Game Functions
+ * ---------------------------------------------------------------------------
+ */
 function gameInitialize() {
     var canvas = document.getElementById("game-screen");  
     context = canvas.getContext("2d");
@@ -21,6 +40,10 @@ function gameInitialize() {
 }
 
 function gameLoop() {
+    gameDraw();
+    snakeUpdate();
+    snakeDraw();
+    foodDraw();
     
 }
 
@@ -30,20 +53,62 @@ function gameDraw() {
     context.fillStyle = "rgb(51,173,189)";
     context.fillRect(0, 0, screenWidth, screenHeight);
 }
+/* ---------------------------------------------------------------------------
+ * Snake Functions
+ * ---------------------------------------------------------------------------
+ */
 
- function snakeIntialize() {
+
+ function snakeInitialize() {
     snake = [];
     snakeLength = 5;
     snakeSize = 20;
+    snakeDirection = "down";
     
-    for(var index = 0; index < snakeLength; index++) {
+    for(var index = snakeLength - 1; index >= 0; index--) {
         snake.push( {x: index, y: 0} );
     }
 }
 
- function snakeDrawI() {
+ function snakeDraw() {
      for(var index  = 0; index < snake.length; index++) {
          context.fillStyle = "white";
          context.fillRect(snake[index].x * snakeSize, snake[index].y * snakeSize, snakeSize, snakeSize);
      }
  }  
+ 
+ function snakeUpdate() {
+     var snakeHeadX = snake[0].x;
+     var snakeHeadY = snake[0].y;
+     
+     if(snakeDirection == "down") {
+         snakeHeadY++;
+     }
+     
+     var snakeTail = snake.pop();
+     snakeTail.x = snakeHeadX;
+     snakeTail.y = snakeHeadY;
+     snake.unshift(snakeTail);
+ }
+ 
+ /*---------------------------------------------------------------------------
+  * Food Functions
+  * --------------------------------------------------------------------------
+  */
+ 
+ function foodInitialize() {
+     food = {
+         x: 0,
+         y: 0
+     };
+ }
+ 
+ function foodDraw() {
+     context.fillStyle = "white";
+     context.fillRect(food.x, food.y, snakeSize, snakeSize);
+ }
+ 
+ function setFoodPosition() {
+     var randomX = Math.floor(Math.random() * (screenWidth));
+     var randomY = Math.floor(Math.random() * (screenHeight);
+ }
